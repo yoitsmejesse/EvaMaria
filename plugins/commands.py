a140c4097e0d56ed1e17b4b42df6f351ab85e0e8
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2022-08-17 07:03:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -228,6 +228,7 @@ async def start(client, message):
         except:
             pass
         return await message.reply('No such file exist.')
+
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
