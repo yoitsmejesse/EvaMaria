15304a4932c28ffa548876a0d09228b8d832de51
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2022-08-20 14:34:18 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -57,16 +57,8 @@ async def answer(bot, query):
         title=file.file_name
         size=get_size(file.file_size)
         f_caption=file.caption
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-                f_caption = f_caption
         if f_caption is None:
-            f_caption = f"<b>{file_name}</b>\n\n ○ <b>Courtesy of <a href=https://t.me/showsarchive>Cine Verse Archive</a></b>"
+            f_caption = f"<b>{file.file_name}</b>\n\n ○ <b>Courtesy of <a href=https://t.me/showsarchive>Cine Verse Archive</a></b>"
         results.append(
             InlineQueryResultCachedDocument(
                 title=file.file_name,
