65ca864d5f34ac794f3abe1b9190b5fbb5bab71a
Jesse Pinkman <95560554+yoitsmejesse@users.noreply.github.com>
2023-06-11 22:52:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,45 +12,76 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = 10012378
+API_HASH = e4b851e683d75f83293ecd51ae135976
+BOT_TOKEN = 5166508400:AAEBwoocaTS-Ij4ilIH-wCpl1J6QQP6wupI
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
+PICS = https://telegra.ph/file/cdeae1b25b5d9799e9cde.jpg
+https://telegra.ph/file/04c637aceecfce8d4af39.jpg
+https://telegra.ph/file/bab9f8b3bdf18acc11991.jpg
+https://telegra.ph/file/5d7175eb1a5d27ab40520.jpg
+https://telegra.ph/file/45333a1182fdd724a3253.jpg
+https://telegra.ph/file/0eebc207b4d46c8779d0d.jpg
+https://telegra.ph/file/5231b4d99e13fc9320682.jpg
+https://telegra.ph/file/e50450c34f13b6d64bb87.jpg
+https://telegra.ph/file/8bf0854c24e5175611365.jpg
+https://telegra.ph/file/95ae0fd0a902d811e614c.jpg
+https://telegra.ph/file/d35e6daf8389f4ba6c033.jpg
+https://telegra.ph/file/55cf3c5a5ef80ae6045c9.jpg
+https://telegra.ph/file/094532fd1fafbe7e0de73.jpg
+https://telegra.ph/file/1574016692fbfe25ee24b.jpg
+https://telegra.ph/file/4850fbbdb1e7c79a459f9.jpg
+https://telegra.ph/file/6f7408fc855f7c0d2f095.jpg
+https://telegra.ph/file/06231a267ecc99b8be7ea.jpg
+https://telegra.ph/file/4927fbbd32f4e1076e7de.jpg
+https://telegra.ph/file/286c0c5975027e0ba0ed1.jpg
+https://telegra.ph/file/2967c25a5e95b627b85d9.jpg
+https://telegra.ph/file/496d3765872638f3d8b84.jpg
+https://telegra.ph/file/44ee0109180558bdc9c3a.jpg
+https://telegra.ph/file/e5c3272ccb1346fe8a724.jpg
+https://telegra.ph/file/c8bff892a7239f7068c3f.jpg
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+ADMINS = 901899557 5238714460
+CHANNELS = -1001489791799
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_CHANNEL = -1001511186264
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_URI = mongodb+srv://alexademiebot:alexademiebot@cluster0.wiern.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+DATABASE_NAME = JinxVerseV2
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = -1001724781682
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
+IMDB = False
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
+CUSTOM_FILE_CAPTION = <b>{file_name}</b>
+
+○ <b>Courtesy of <a href=https://t.me/showsarchive>Cine Verse Archive</a></b>
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+IMDB_TEMPLATE = <b>{title} | {year} • {kind}</b>
+
+<b>Rating ⭐️: {rating} / 10 on IMDb</b>
+<b>Runtime: {runtime} Minutes</b>
+<b>Genres: {genres}</b>
+
+<b>Language: {languages} | Subbed</b>
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+SPELL_CHECK_REPLY = False
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
+MELCOW_NEW_USERS = False
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
